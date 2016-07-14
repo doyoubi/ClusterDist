a9318818d75787fa541849604db965d2dfb29e7c
doyoubi <doyoubihgx@gmail.com>
2016-07-14 10:09:33 +0000

    added class

--- dist.py
+++ dist.py
@@ -3,12 +3,25 @@ import operator
 from itertools import repeat, izip_longest
 
 class Node(object):
-    def __init__(self, tag, host, port, master=None):
+    def __init__(self, tag, host, master=None):
         self.tag = tag
         self.host = host
-        self.port = port
         self.master = master
 
+class Machine(object):
+    def __init__(self, tag, masters, slaves, free_nodes):
+        self.tag = tag
+        self.masters = masters
+        self.slaves = slaves
+        self.free_nodes = free_nodes
+
+    def to_dict(self):
+        return {
+            'tag': self.tag,
+            'masters': self.masters,
+            'slaves': self.slaves,
+            'frees': self.free_nodes,
+        }
 
 def gen_cluster(master_count, slave_count, machine_count):
     machines = list(range(machine_count))
@@ -16,23 +29,44 @@ def gen_cluster(master_count, slave_count, machine_count):
     slaves = [list() for i in range(machine_count)]
     for i in range(master_count):
         m = random.choice(machines)
-        masters[m].append(i)
+        masters[m].append(Node(i, m))
     for i in range(master_count, master_count + slave_count):
         m = random.choice(machines)
-        slaves[m].append(i)
+        other_m = []
+        while len(other_m) == 0:
+            other_m = random.choice(masters[:m] + masters[m+1:])
+        slaves[m].append(Node(i, m, random.choice(other_m)))
     return masters, slaves
 
 def print_cluster(masters, slaves, machine_count):
-    ms = [['*{}'.format(n) for n in m] for m in masters]
-    ss = [['#{}'.format(n) for n in m] for m in slaves]
+    ms = [['*{}'.format(n.tag) for n in m] for m in masters]
+    ss = [['#{}-{}'.format(n.tag, n.master.tag) for n in m] for m in slaves]
     all_node = map(operator.add, ms, ss)
     for line in izip_longest(*all_node, fillvalue=' '): 
         print '\t'.join(repeat('{}', machine_count)).format(*line)
 
-ms, ss = gen_cluster(3, 5, 3)
-print_cluster(ms, ss, 3)
+machine_count = 3
+ms, ss = gen_cluster(3, 5, machine_count)
+print_cluster(ms, ss, machine_count)
+
+def gen_free_nodes(machine_count, num):
+    machines = list(range(machine_count))
+    nodes = [list() for i in range(machine_count)]
+    for i in range(num):
+        m = random.choice(machines)
+        nodes[m].append(Node(i, m))
+    return nodes
+
+free_nodes = gen_free_nodes(machine_count, 3)
+
+machines = [Machine(i, ms[i], ss[i], free_nodes[i]) for i in range(machine_count)]
+    
+for d in map(Machine.to_dict, machines):
+    print d
+
+def dist_slave(machines, free_count):
+    machines = sorted(machines, key=lambda m: len(m.masters))
 
 
-def dist(masters, slaves, add_master, add_slaves):
-    pass
+dist_slave(machines, len(free_nodes))
 
